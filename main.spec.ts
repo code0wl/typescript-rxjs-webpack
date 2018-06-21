3518c0057cede19290911b968d1f970b070613d3
code0wl <o.lodriguez@gmail.com>
2018-06-21 20:08:17 +0200

    jest config

--- main.spec.ts
+++ main.spec.ts
@@ -5,9 +5,9 @@ describe("rxjs-marbles", () => {
 
     it("should support marble tests", marbles(m => {
 
-        const source =  m.hot("--^-a-b-c-|");
-        const subs =            "^-------!";
-        const expected =        "--b-c-d-|";
+        const source = m.hot("--^-a-b-c-|");
+        const subs = "^-------!";
+        const expected = "--b-c-d-|";
 
         const destination = source.pipe(
             map(value => String.fromCharCode(value.charCodeAt(0) + 1))
