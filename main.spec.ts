254c9d36988fc206e8f3adcf451f53e3144bdf71
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-04-25 07:11:09 +0200

    version compliancy

--- main.spec.ts
+++ main.spec.ts
@@ -2,17 +2,18 @@ import { marbles } from "rxjs-marbles/mocha";
 import { map } from "rxjs/operators";
 
 describe("rxjs-marbles", () => {
+  it(
+    "should support marble tests",
+    marbles(m => {
+      const source = m.hot("--^-a-b-c-|");
+      const subs = "^-------!";
+      const expected = "--b-c-d-|";
 
-    it("should support marble tests", marbles(m => {
-
-        const source = m.hot("--^-a-b-c-|");
-        const subs = "^-------!";
-        const expected = "--b-c-d-|";
-
-        const destination = source.pipe(
-            map(value => String.fromCharCode(value.charCodeAt(0) + 1))
-        );
-        m.expect(destination).toBeObservable(expected);
-        m.expect(source).toHaveSubscriptions(subs);
-    }));
+      const destination = source.pipe(
+        map(value => String.fromCharCode(value.charCodeAt(0) + 1))
+      );
+      m.expect(destination).toBeObservable(expected);
+      m.expect(source).toHaveSubscriptions(subs);
+    })
+  );
 });
