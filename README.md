423bd87716a9390a984c0e13fc6007568c8f824b
Oscar Lodriguez (OSV) <oscar.lodriguez.osv@fedex.com>
2020-07-21 21:02:29 +0200

    watch

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Solid base for practising RxJS
 
+[![Build Status](https://travis-ci.org/code0wl/typescript-rxjs-webpack.svg?branch=master)](https://travis-ci.org/code0wl/typescript-rxjs-webpack)
+
 Use this playground to quickly scaffold a rxjs program that includes marble testing for scratchpad purposes
 
 ## Usage
@@ -11,7 +13,7 @@ npm start
 ## TDD
 
 ```bash
-npm test
+npm run test:watch
 ```
 
 Project will start on the specified port with watch mode, enjoy! 
--- main.ts
+++ main.ts
@@ -1,18 +1,15 @@
 import { interval } from "rxjs";
-import { scan } from "rxjs/operators/";
+import { scan } from "rxjs/operators";
 
 // ****
-//
 // Welcome to your rxjs scratch pad 🤗
-//
 // ****
 
 const source = interval(2000);
-
-const sourceMessage = x => `rxjs logo spun ${ x } times`;
 const messageElement = document.querySelector('.message');
 
-const log = x => (messageElement.textContent = x);
+const sourceMessage = x => `rxjs logo spun ${ x } times`;
+const log = x => messageElement.textContent = x;
 
 source
   .pipe(scan((_, count) => count + 1, 0))
